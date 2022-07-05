7989f9fc4da763c62c97816782cbbb93ad8c82e2
shin_jong_ho <sjh0804_22@naver.com>
2022-07-05 17:19:13 +0900

    07

---
+++ main_program/__main__.py
@@ -0,0 +1,312 @@
+from flask import Flask, request, render_template
+import threading
+import start
+from datetime import datetime
+import datetime
+import json
+app = Flask(__name__)
+import matplotlib.pyplot as plt
+import random
+import os, glob, time
+import start
+from tqdm import tqdm
+
+import csv
+
+from utils.processing_json import Processing_json
+from utils.predict import Predict 
+
+#누적 그래프 결과 페이지
+@app.route('/resultPage', methods=['GET', 'POST'])
+def result() :
+    if request.method == 'GET':
+        l = []
+        s = []
+        e = []
+        # csv파일 읽어서 html로 값 전송
+        with open(f'C:/Users/g/ACIN_public/00_data/main_program/static/search.csv', 'r',newline='', encoding='utf8') as f:
+            re = csv.reader(f)
+            for i in re :
+                l.append(i[0]+i[1]+i[2])
+            l.reverse()
+            print(l)
+        return render_template("./resultPage.html", search_list = l, start_day = s, end_day = e)
+
+#사이트 설명 페이지
+@app.route('/guide', methods=['GET', 'POST'])
+def guide() :
+    if request.method == 'GET':
+        return render_template("./guide.html")
+#모델 실행이 끝난 후 그래프 보여주는 페이지
+@app.route('/graph', methods=['GET', 'POST'])
+def graph():
+    
+    file = f'C:/Users/g/ACIN_public/00_data/main_program/result/naver_news/news_{search}_naver_{start_date}_{end_date}__{end_date[:6]}.json'
+    while True :
+        if os.path.isfile(file) :
+            print("-"*30)
+            print("[ json파일이 생성되었습니다. ]")
+            print("-"*30)
+            print("[ 감성분석을 시작합니다. ]")
+            print("-"*30)
+
+            ## 전처리 후 예측
+            processing = Processing_json(f'C:/Users/g/ACIN_public/00_data/main_program/result/naver_news/news_{search}_naver_{start_date}_{end_date}__{end_date[:6]}.json')
+            processed_dic = processing.dateNList()
+
+            print(processed_dic)
+
+            def dic_to_result(processed_dic):   
+                predict = Predict()
+
+                result_dic = {}
+                result_happy = []
+                result_bad = []
+
+                for key in processed_dic.keys():
+                    missing_value = False
+                    positive = 0
+                    negative = 0
+                    if len(processed_dic[key]) == 0:
+                        missing_value = True
+                    for comment in processed_dic[key]:
+                        if predict.predict(comment):
+                            positive +=1
+                        else:
+                            negative +=1
+                    if missing_value:
+                        result_dic[key] = -1
+                    else:
+                        result_dic[key] = positive/(positive+negative)*100
+                    
+                    result_happy.append(positive)
+                    result_bad.append(negative)
+                    
+                    positive = 0
+                    negative = 0
+                return result_dic,result_happy,result_bad   #ex) {'20220623':70, '20220624':-1(결측값)}
+            
+            result,happy_num,bad_num = dic_to_result(processed_dic)
+            all_num = [x+y for x,y in zip(happy_num, bad_num)]
+
+            print("감성분석 결과 입니다.")
+            print(result)
+            print(happy_num)
+            print(bad_num)
+            print(all_num)
+
+            # 그래프 그리기--------------------------------------------
+            startDate = start_date
+            lastDate = end_date
+
+            # 각 날짜를 리스트에 끊어서 저장
+            # ex) 20220627 = ['2', '0', '2', '2', '0', '6', '2', '7']
+            start_dateList = []
+            for y in (startDate):
+                start_dateList.append(y)
+            last_dateList = []
+            for y in (lastDate):
+                last_dateList.append(y)
+
+            # string 타입의 리스트를 int 타입으로 변환
+            start_dateList = list(map(int, start_dateList))
+            last_dateList = list(map(int, last_dateList))
+
+            # 시작 날짜 정리
+            thousand = start_dateList[0] * 1000
+            hundred = start_dateList[1] * 100
+            yten = start_dateList[2] * 10
+            yone = start_dateList[3]
+            mten = start_dateList[4] * 10
+            mone = start_dateList[5]
+            dten = start_dateList[6] * 10
+            done = start_dateList[7]
+            startYear = thousand + hundred + yten + yone
+            startMonth = mten + mone
+            startDay = dten + done
+
+            # 마지막 날짜 정리
+            thousand = last_dateList[0] * 1000
+            hundred = last_dateList[1] * 100
+            yten = last_dateList[2] * 10
+            yone = last_dateList[3]
+            mten = last_dateList[4] * 10
+            mone = last_dateList[5]
+            dten = last_dateList[6] * 10
+            done = last_dateList[7]
+            lastYear = thousand + hundred + yten + yone
+            lastMonth = mten + mone
+            lastDay = dten + done
+
+            # datetime으로 적용
+            startday = datetime.date(startYear, startMonth, startDay)
+            lastday = datetime.date(lastYear, lastMonth, lastDay)
+
+            # 두 날짜간 차이 계산
+            dateResult = startday - lastday
+            dateResult = abs(dateResult)
+
+            # 날짜형 -> 문자형 변환 후 날짜간 차이를 정수형으로 저장
+            strDate = str(dateResult)
+            sstrDate = strDate.split(' ')
+            intDate = int(sstrDate[0])      # 날짜간 차이(정수형)
+
+            monthList = []      # 사용자가 지정한 month의 리스트
+            dateList = []       # 사용자가 지정한 day의 리스트
+            yearList = []
+
+            #   1월 2월 3월 4월  5월 6월 7월 8월 9월 10월 11월 12월
+            m = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31,  30,  31]
+
+            # 같은 달일때
+            if startMonth == lastMonth and intDate < m[startMonth-1] :
+                for i in range(startDay,lastDay+1) :
+                    dateList.append(i)
+                    monthList.append(startMonth)
+                    yearList.append(startYear)
+            # 다른 달일때
+            elif startMonth != lastMonth :
+                # 다른 연도
+                if startYear != lastYear :
+                    m_m = lastMonth - startMonth
+                    m_m = abs(abs(m_m)-12)+1
+                    for i in range(m_m) :
+                        if i == 0 :
+                            for j in range(startDay,m[((startMonth-1)+i)-12]+1):
+                                dateList.append(j)
+                                monthList.append((startMonth-1)+i+1)
+                                yearList.append(startYear)
+
+                        elif 0<i<m_m-1:
+                            for j in range(1,m[((startMonth-1)+i)-12]+1):
+                                dateList.append(j)
+                                monthList.append(i)
+                                
+                                if ((startMonth-1)+i-12+1) < startMonth :
+                                    yearList.append(lastYear)
+                                
+
+                        else :
+                            for j in range(1,lastDay+1):
+                                dateList.append(j)
+                                yearList.append(lastYear)
+                                monthList.append(i)            
+                # 같은 연도
+                else :
+                    m_m = lastMonth - startMonth +1
+                    for i in range(m_m) :
+                        if i == 0 :
+                            for j in range(startDay,m[startMonth-1]+1):
+                                dateList.append(j)
+                                monthList.append(startMonth)
+                                yearList.append(startYear)
+                        elif 0<i<m_m-1:
+                            for j in range(1,m[startMonth-1+i]+1):
+                                dateList.append(j)
+                                monthList.append((startMonth-1)+1)
+                                yearList.append(startYear)
+                        else :
+                            for j in range(1,lastDay+1):
+                                dateList.append(j)
+                                monthList.append((lastMonth-1)+1)
+                                yearList.append(startYear)
+
+            # 정수형 리스트를 문자열 리스트로 변환
+            str_month_list = list(map(str, monthList))
+            str_date_list = list(map(str, dateList))
+            str_year_list = list(map(str, yearList))
+
+            # n월 n일 형태로 출력
+            listLen = len(str_month_list)
+            resultList = []
+            for i in range(listLen):
+                if len(str_month_list[i]) != 2 and len(str_date_list[i]) != 2 :
+                    resultList.append(str_year_list[i]+"0"+str_month_list[i]+ "0"+str_date_list[i])
+                elif len(str_month_list[i]) != 2 and len(str_date_list[i]) == 2 :
+                    resultList.append(str_year_list[i]+"0"+str_month_list[i]+str_date_list[i])
+                elif len(str_month_list[i]) == 2 and len(str_date_list[i]) != 2 :
+                    resultList.append(str_year_list[i]+str_month_list[i]+"0"+str_date_list[i])
+                else :
+                    resultList.append(str_year_list[i]+str_month_list[i]+ str_date_list[i])
+
+            #html에 보내줄 값 저장
+            happy = happy_num
+            bad = bad_num
+            all_n = all_num
+            search_day = search + start_date + end_date
+            print(resultList)
+
+            #csv저장 = DB역할 
+            with open(f'C:/Users/g/ACIN_public/00_data/main_program/static/search.csv', 'a',newline='', encoding='utf8') as f:
+                wr = csv.writer(f)
+                wr.writerow([search,start_date,end_date])
+            
+            #그래프 그린후 저장
+
+            plt.clf()
+            #긍정 그래프
+            plt.plot(resultList,happy,color='blue',linestyle='-',marker='o')
+            #plt.plot(resultList,bad,color='red',linestyle='-',marker='o')
+            plt.xticks(resultList, rotation='90')  # x축 라벨의 이름 pow지움
+            plt.title(f'{search} p graph', )  # 그래프 제목 설정
+            plt.ylabel('happy_num',)  # y축에 설명 추가
+            plt.tight_layout()
+            #plt.ylim(0,max(max(happy),max(bad)))
+            #C:\Users\g\ACIN_public\00_data\main_program\static\images
+            plt.savefig(f'C:/Users/g/ACIN_public/00_data/main_program/static/images/{search}{start_date}{end_date}happy.jpg')
+            plt.clf()
+            # 부정 그래프
+            plt.plot(resultList,bad,color='red',linestyle='-',marker='o')
+            plt.xticks(resultList, rotation='90')  # x축 라벨의 이름 pow지움
+            plt.title(f'{search} n graph', )  # 그래프 제목 설정
+            plt.ylabel('bad_num',)  # y축에 설명 추가
+            plt.tight_layout()
+            plt.savefig(f'C:/Users/g/ACIN_public/00_data/main_program/static/images/{search}{start_date}{end_date}bad.jpg')
+            plt.clf()
+            # 관심도 그래프
+            plt.plot(resultList,all_n,color='green',linestyle='-',marker='o')
+            plt.xticks(resultList, rotation='90')  # x축 라벨의 이름 pow지움
+            plt.title(f'{search} interest index graph', )  # 그래프 제목 설정
+            plt.ylabel('interest index',)  # y축에 설명 추가
+            plt.tight_layout()
+            plt.savefig(f'C:/Users/g/ACIN_public/00_data/main_program/static/images/{search}{start_date}{end_date}all.jpg')
+            plt.clf()
+            return render_template("./graph_page.html", value = result,happy_value = happy_num,bad_value = bad_num, value_search = search, search_day = search_day)
+
+#중간 로딩 페이지
+@app.route('/loding', methods=['GET', 'POST'])
+def lode():
+    if request.method == 'GET':
+        return render_template("./loding.html")
+
+#처음 시작 페이지
+@app.route('/', methods=['GET', 'POST'])
+def goo():
+    
+    if request.method == 'GET':
+        return render_template("./index.html")
+    elif request.method == 'POST':
+        global search
+        global start_date
+        global end_date
+        global file
+        search = request.form.get('keyword')
+        start_date = request.form.get('start_date')
+        end_date = request.form.get('end_date')
+        
+        start_date = start_date[:4] + start_date[5:7] + start_date[8:]
+        end_date = end_date[:4] + end_date[5:7] + end_date[8:]
+        print("[검색어, 날짜가 입력 되었습니다.]")
+        print(search, start_date, end_date)
+
+        #크롤러 실행 
+        file = f'C:/Users/g/ACIN_public/00_data/main_program/result/naver_news/news_{search}_naver_{start_date}_{end_date}__{end_date[:6]}.json'
+        threading.Thread(target=start.main, args=(search, start_date, end_date,)).start()
+        print("[ 스레드 크롤러가 실행 되었습니다. ]")
+
+        
+        return render_template("./loding.html", search = search ,start_date = start_date,end_date = end_date,file = file)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="80",debug=False, threaded=True )
+
---
+++ main_program/start.py
@@ -0,0 +1,209 @@
+from gevent import monkey as curious_george
+curious_george.patch_all(thread=False, select=False)
+
+from selenium import webdriver
+# 싱글프로세싱
+# from crawlers.DaumNewsCrawler import DaumCrawler
+# from crawlers.NaverNewsCrawler import NaverCrawler
+
+# 멀티프로세싱
+# from crawlers.DaumNewsMultiCrawler import crawlLinks as daumCrawlLinks, crawlNews as daumCrawlNews
+from crawlers.NaverNewsMultiCrawler import crawlLinks as naverCrawlLinks, crawlNews as naverCrawlNews
+
+from utils.util import *
+
+import json
+import datetime
+
+import numpy as np
+ 
+from tqdm import trange
+
+import sys
+sys.setrecursionlimit(5000)
+
+from utils.processing_json import Processing_json
+from utils.predict import Predict 
+
+
+def info_time(dic, us_news, kr_news):
+    dic_key = dic.keys()
+    info_term_time = []
+
+    date_dic = {}
+
+    for i in dic_key:
+        temp_time = [x.split('.')[1] for x in dic[i]]
+
+        if len(temp_time) > 1:
+            temp_time.sort()
+            sum_time = 0
+
+            for j in temp_time:
+                '''
+                j = kr_news[j]['date']
+                year_ = int(j[:4])
+                mon_ = int(j[4:6])
+                day_ = int(j[6:8])
+                '''
+                hour_ = int(j[8:10])
+                min_ = int(j[10:12])
+                sec_ = int(j[12:14])
+                sum_time += hour_ * 60 * 60 + min_ * 60 + sec_
+            avg_time = sum_time / len(temp_time)
+
+            end_year = temp_time[-1][:4]
+            end_mon = temp_time[-1][4:6]
+            end_day = temp_time[-1][6:8]
+            end_hour = temp_time[-1][8:10]
+            end_min = temp_time[-1][10:12]
+            end_ = ''.join([
+                str(i)
+                for i in [end_year, end_mon, end_day, end_hour, end_min
+            ]])
+
+            start_year = temp_time[0][:4]
+            start_mon = temp_time[0][4:6]
+            start_day = temp_time[0][6:8]
+            start_hour = temp_time[0][8:10]
+            start_min = temp_time[0][10:12]
+            start_ = ''.join([
+                str(i)
+                for i in [start_year, start_mon, start_day, start_hour, start_min
+            ]])
+
+            diff = (datetime(
+                    int(end_year),
+                    int(end_mon),
+                    int(end_day),
+                    int(end_hour),
+                    int(end_min)
+                )  -  datetime(
+                    int(start_year),
+                    int(start_mon),
+                    int(start_day),
+                    int(start_hour),
+                    int(start_min)
+                )).total_seconds()
+            info_term_time.append([
+                i,
+                us_news[i]['title'],
+                us_news[i]['date'],
+                start_,
+                end_,
+                diff,
+                avg_time
+            ])
+
+            this_date = us_news[i]['date'][:8]
+
+            if this_date in date_dic.keys():
+                date_dic[this_date].append(avg_time)
+            else:
+                date_dic[this_date] = [avg_time]
+
+    for k, v in date_dic.items():
+        date_dic[k] = round(np.mean(v)/3600, 1)
+
+    return date_dic
+
+    # dataframe = df(info_term_time, columns=['URL', 'Title', '미국기사 시간', '처음 한국기사 시간', '마지막 한국기사 시간', 'diff', '평균'])
+    # dataframe.to_csv('Task2.csv', encoding='cp949')
+
+def main(search, start_date, end_date):
+    #if __name__ == "__main__":
+    # 크롬 드라이버 링크
+    driver_url = './chromedriver.exe'
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--privileged')
+    chrome_options.add_argument('--incognito')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+
+    chrome_options.add_argument('window-size=1920x1080')
+    chrome_options.add_argument('disable-gpu')
+    chrome_options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
+    chrome_options.add_argument('lang=ko_KR')
+
+    # selenium으로 크롤링하여 저 장된 링크를 가지고 requests로 다시 크롤링하여 json으로 저장
+    # kr은 requests로 동기식, us는 grequests와 async로 비동기식 (kr은 비동기가 안먹힘(다음뉴스 500 오류))
+
+    #search = search # 검색할 키워드
+    #start_date = start_date # 검색 기간 시작 날짜
+    #end_date = end_date # 검색 기간 종료 날짜
+    
+    search = search.replace(' ', '+')
+
+
+    """ 싱글 프로세싱 코드
+
+    # # 크롤러 객체 생성
+    # daum_crawler = DaumCrawler(driver_url, chrome_options)
+    # naver_crawler = NaverCrawler(driver_url, chrome_options)
+
+    # daum_crawler.crawlLinks(search, start_date, end_date) # 링크 크롤링(selenium)
+    # daum_crawler.crawlNews(search, start_date, end_date) # 뉴스 크롤링(async+grequest+bs4)
+    
+    # naver_crawler.crawlLinks(search, start_date, end_date) # 링크 크롤링(selenium)
+    # naver_crawler.crawlNews(search, start_date, end_date) # 뉴스 크롤링(async+grequest+bs4)
+    """
+
+
+    # 멀티 프로세싱 코드  -- 다음 블라인드 때문에 동작x
+    # daumCrawlLinks(search, start_date, end_date, driver_url, chrome_options)
+    # daumCrawlNews(search, start_date, end_date, driver_url, chrome_options)
+    
+    naverCrawlLinks(search, start_date, end_date, driver_url, chrome_options)
+    naverCrawlNews(search, start_date, end_date, driver_url, chrome_options)
+
+    dic = {}
+    dic2 = {}
+        
+    start_date_ = datetime.date(int(start_date[:4]), int(start_date[4:6]), int(start_date[6:]))
+    end_date_ = datetime.date(int(end_date[:4]), int(end_date[4:6]), int(end_date[6:])) + datetime.timedelta(days=1)
+
+    date_list = [str(i).replace('-', '')[0:8] for i in daterange(start_date_, end_date_)]
+
+
+    for date in date_list:
+        if date not in dic.keys():
+            dic[date] = {}
+
+    all = 0
+    for date in dic.keys():
+        for company in ["naver"]: #daum은 따로
+            with open(f'result/{company}_news/news_{search}_{company}_{start_date}_{end_date}__{date[:6]}.json','r', encoding='utf8') as f:
+                dic2 = json.load(f)
+
+            dic[date].update(dic2[date])
+
+
+    dic2 = {}
+    for date in dic.keys():
+        if date[:6] not in dic2.keys():
+            dic2[date[:6]] = []
+
+        dic2[date[:6]].append(dic[date])
+
+
+    all = 0
+    for mon in dic2.keys():
+        count = 0
+        for dic3 in dic2[mon]:
+            for url, contain in dic3.items():
+                for comment in contain['comments']:
+                    count += 1
+        all += count
+        print(f'{mon} : {count}')
+    print(f'all : {all}')
+
+    # with open(f'result/news_{search}_all_{start_date}_{end_date}.json', 'w', encoding='utf8') as f:
+    #     json.dump(dict(dic), f, indent=4, sort_keys=True, ensure_ascii=False)
+    
+    exit()
+
+
+
+    
